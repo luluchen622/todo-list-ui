a382b011122508c32b99005c9e3669372b2008dc
luluchen622 <lulu19930622@gmail.com>
2023-05-02 21:51:46 +0800

    自定義hook

---
+++ src/vue3/components/Demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>座標X {{point.x}}, 座標Y {{point.y}}</h2>
+</template>
+
+<script>
+import usePoint from "@/vue3/hooks/usePoint";
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Demo",
+  setup(){
+    let point = usePoint();
+    return {point}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
